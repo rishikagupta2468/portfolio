e86fc443f0ece7ead2f11ab19fd8d6e667b7841d
Rishika Gupta <rishikagupta246813579@gmail.com>
2020-07-05 23:44:36 +0530

    about me and experience

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -31,6 +31,23 @@
         $(this).addClass('activate');
         $(this).siblings().removeClass('activate');
     });
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        if(value == "all")
+        {
+            $('.filter').show('1000');
+        }
+        else
+        {
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');        
+        }
+    });
+    
+    if ($(".filter-button").removeClass("active")) {
+        $(this).removeClass("active");
+        }
+        $(this).addClass("active");
  });
 
 
