aa0a5a9ea1009b71891b29e34109594f1b6c95a3
Rishika Gupta <rishikagupta246813579@gmail.com>
2020-07-11 17:18:21 +0530

    updates

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -23,6 +23,17 @@
     if ($(".filter-button").removeClass("active")) {
         $(this).removeClass("active");
     }
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 1000, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
  });
 
 
